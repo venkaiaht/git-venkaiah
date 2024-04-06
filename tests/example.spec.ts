5cd55cb1830bf7d389f3b23a8f6cca45b6398de1
venkaiah <venkaiaht14@gmail.com>
2024-04-07 00:06:54 +0530

    new changes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://google.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/google/);
 });
 
 
+//i am venkaiah from nellore
