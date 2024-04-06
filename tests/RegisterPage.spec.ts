81769edea585665a1b083974d17ea1a0189372ec
venkaiah <venkaiaht14@gmail.com>
2024-04-07 01:14:07 +0530

    loginpage

--- tests/RegisterPage.spec.ts
+++ tests/RegisterPage.spec.ts
@@ -1,5 +1,9 @@
-var sa = 29
-var bb = 33
-var c = sa + bb;
-console.log(c)
-//venkaiah swamy
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+    await page.goto('https://www.facebook.com/');
+    await page.getByTestId('royal_email').fill('thammisettivenkaiah');
+    await page.locator('#passContainer').click();
+    await page.getByTestId('royal_pass').fill('chandana@143');
+    await page.getByTestId('royal_login_button').click();
+});
